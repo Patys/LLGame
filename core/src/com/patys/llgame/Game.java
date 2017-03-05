834be4fab7729ae781c0e7a60c19631238846fd3
Patryk Szczygło <patysprog@gmail.com>
2017-03-05 16:44:26 +0100

    resolved issues with android target and added basic camera

--- core/src/com/patys/llgame/Game.java
+++ core/src/com/patys/llgame/Game.java
@@ -3,17 +3,29 @@ package com.patys.llgame;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	
+	final float appWidth = 720; 
+	final float appHeight = 1280;
+	float screenWidth;
+	float screenHeight;
+	
+	OrthographicCamera camera;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		screenWidth = Gdx.graphics.getWidth();
+		screenHeight = Gdx.graphics.getHeight();
+		
+  		camera = new OrthographicCamera();
+		camera.setToOrtho(false, appWidth, appHeight);
 	}
 
 	@Override
@@ -21,13 +33,19 @@ public class Game extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+
 		batch.end();
 	}
+
+	@Override
+	public void resize(int width, int height) {
+		screenWidth = width;
+		screenHeight = height;
+        camera.update();
+    }
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
