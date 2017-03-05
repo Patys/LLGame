dddae64436f97b41aaf0ac07e5c68dcd419bb1a7
Patryk Szczygło <patysprog@gmail.com>
2017-03-05 16:04:24 +0100

    generated project

---
+++ android/src/com/patys/llgame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.patys.llgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.patys.llgame.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
